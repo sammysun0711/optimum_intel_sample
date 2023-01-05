872accf1465b7112c4b3f9d8b4d5ac1c005f4cc1
Xiake Sun <xiake.sun@intel.com>
2023-01-05 13:55:16 +0800

    Update enable_overflow_fix function

--- utils.py
+++ utils.py
@@ -6,9 +6,9 @@ def enable_overflow_fix():
     w = "without"
     overflow_fix = 'enable'
     for flag in flags:
-        if 'vnni' or 'amx_int8' in flag:
+        if 'vnni' in flag or 'amx_int8' in flag:
             w = "with"
             overflow_fix = 'disable'
-    print("Detected CPU platform {} {} Intel(R) Deep Learning Boost (VNNI) technology and further generations, {}d overflow fix".format(brand_raw, w, overflow_fix))
+    print("Detected CPU platform {} {} support of Intel(R) Deep Learning Boost (VNNI) technology and further generations, {}d overflow fix".format(brand_raw, w, overflow_fix))
 
     return overflow_fix
